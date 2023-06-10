209ee7b5774a15a2b512c73343c0e3166e70b73f
Nuriye <nuriyeerdogan38@gmail.com>
2023-06-10 17:44:37 +0200

    Oprettet 2 klasser, lokale og måling. Tiljøjet to string samt constructors.

---
+++ ZelandClimate/Lokale.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZelandClimate
+{
+    internal class Lokale
+    {
+        public int id;
+        public string lokaleNummer;
+        public int maxAntal;
+        
+        // constructors
+
+        public Lokale(int id, string lokaleNummer, int maxAntal)
+        {
+
+            ID = id;
+            LokaleNummer = lokaleNummer;
+            MaxAntal = maxAntal;
+
+
+        }
+
+        public int ID { get; set; }
+        public string LokaleNummer { get; set; }
+        public int MaxAntal { get; set; }
+
+
+
+        public override string ToString()
+        {
+            return $"Lokale's info: {ID}, {LokaleNummer}, {MaxAntal}";
+        }
+    }
+
+
+}
---
+++ ZelandClimate/Måling.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZelandClimate
+{
+    internal class Måling
+    {
+        //properties
+        public int id;
+        public DateTime dateTime;
+        public double temp;
+        public int co2;
+        public int antalPersoner;
+
+        //constructors
+        public Måling(int id, DateTime dateTime, double temp, int co2, int antalPersoner)
+        {
+            ID = id;
+            DateTime = dateTime;
+            Temp = temp;
+            Co2 = co2;
+            AntalPersoner = antalPersoner;
+
+        }
+        // get set
+        public int ID { get; set; }
+        public DateTime DateTime { get; set; }
+        public double Temp { get; set; }   
+        public int Co2 { get; set; }
+        public int AntalPersoner { get; set; }
+
+        // to string method
+        public override string ToString()
+        {
+            return $"Måling's info: {ID}, {DateTime}, {Temp}, {Co2}, {AntalPersoner}";
+        }
+    }
+
+}
